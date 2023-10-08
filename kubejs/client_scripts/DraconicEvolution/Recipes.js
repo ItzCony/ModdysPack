86e8a5f6de1b2a87c05fc54ba72c76cc17903a83
Cony <97829514+ItzCony@users.noreply.github.com>
2023-10-08 12:13:02 +0200

    Aktualizace modpacku
    Odebráno >>
    
    Celestial Manipulator (Draconic Evolution)
    Mob Grinder (Draconic Evolution)
    Watch of Flowing Time (ProjectE)
    Repair Talisman (ProjectE)
    Destruction Catalyst (ProjectE)
    Catalytic Lens (ProjectE)
    Evertide Amulet (ProjectE)
    Volcanite Amulet (ProjectE)
    Gem of Eternal Density (ProjectE)
    Mercurial Eye (ProjectE)
    Water Condensator (Industrial Forgegoing)
    Block Breaker (Industrial Forgegoing)

--- kubejs/client_scripts/DraconicEvolution/Recipes.js
+++ kubejs/client_scripts/DraconicEvolution/Recipes.js
@@ -1,3 +1,14 @@
 onEvent('rei.hide.items', event => {
-	event.hide('projecte:watch_of_flowing_time')
+	event.hide('projecte:watch_of_flowing_time'),
+  event.hide('draconicevolution:celestial_manipulator')
+  event.hide('draconicevolution:grinder')
+  event.hide('projecte:repair_talisman')
+  event.hide('projecte:destruction_catalyst')
+  event.hide(Item.of('projecte:catalytic_lens', '{StoredEMC:0L}'))
+  event.hide('projecte:evertide_amulet')
+  event.hide('projecte:volcanite_amulet')
+  event.hide('projecte:gem_of_eternal_density')
+  event.hide('projecte:mercurial_eye')
+  event.hide('industrialforegoing:water_condensator')
+  event.hide('industrialforegoing:block_breaker')
   })
